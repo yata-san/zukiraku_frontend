00aa6972b96fc5d1984fad3f845efc12646fdac3
yata-san <yatagai49@gmail.com>
2025-04-13 01:54:41 +0900

    src\app内のcomponentsとcustomersの全てのファイルをコメントアウト

--- src/app/customers/read/[id]/back_button.jsx
+++ src/app/customers/read/[id]/back_button.jsx
@@ -1,14 +1,14 @@
-"use client";
-import { useRouter } from "next/navigation";
+// "use client";
+// import { useRouter } from "next/navigation";
 
-const BackButton = (props) => {
-  const router = useRouter();
+// const BackButton = (props) => {
+//   const router = useRouter();
 
-  return (
-    <button onClick={() => router.back()}>
-      <div className="btn btn-primary m-4 text-2xl">{props.children}</div>
-    </button>
-  );
-};
+//   return (
+//     <button onClick={() => router.back()}>
+//       <div className="btn btn-primary m-4 text-2xl">{props.children}</div>
+//     </button>
+//   );
+// };
 
-export default BackButton;
+// export default BackButton;
