bf2e59ab0c848d26a9181048f592d57312ec81f6
yata-san <yatagai49@gmail.com>
2025-04-13 02:40:51 +0900

    src\app内のcustomersフォルダを削除

--- src/app/customers/create/confirm/fetchCustomer.js
+++
@@ -1,9 +0,0 @@
-// export default async function fetchCustomer(id) {
-//   const res = await fetch(`http://localhost:8000/customers?customer_id=${id}`, {
-//     cache: "no-cache",
-//   });
-//   if (!res.ok) {
-//     throw new Error("Failed to fetch customer");
-//   }
-//   return res.json();
-// }
